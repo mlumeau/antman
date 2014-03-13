123d4e273600de848fb5385cbd04daa49d44c2bc
alexisdevise <alexis.devise@gmail.com>
2014-03-13 10:50:33 +0100

    Ctrl

--- src/main/java/org/ICE/PDC/antman/controller/MainCtrl.java
+++ src/main/java/org/ICE/PDC/antman/controller/MainCtrl.java
@@ -3,7 +3,15 @@
  */
 package org.ICE.PDC.antman.controller;
 
+import javax.swing.event.EventListenerList;
+
+import org.ICE.PDC.antman.model.Eclaireuse;
+import org.ICE.PDC.antman.model.Fourmiliere;
+import org.ICE.PDC.antman.model.MapListener;
+import org.ICE.PDC.antman.model.Monde;
+import org.ICE.PDC.antman.model.Reine;
 import org.ICE.PDC.antman.view.MainFrame;
+import org.apache.log4j.Logger;
 
 /** 
  * <!-- begin-UML-doc -->
@@ -18,7 +26,14 @@ public class MainCtrl {
 	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	 */
 	private MainFrame mainFrame;
+	private Monde monde;
 
+	private EventListenerList listeners;
+	
+	public MainCtrl(int dimension_x,int dimension_y,int meteo,int abondance){
+		monde = new Monde(dimension_x, dimension_y, meteo, abondance);
+	}
+	 
 	/** 
 	 * @return mainFrame
 	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
@@ -30,7 +45,7 @@ public class MainCtrl {
 	}
 
 	/** 
-	 * @param mainFrame mainFrame � d�finir
+	 * @param mainFrame mainFrame à définir
 	 * @generated "UML vers Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	 */
 	public void setMainFrame(MainFrame mainFrame) {
@@ -38,4 +53,20 @@ public class MainCtrl {
 		this.mainFrame = mainFrame;
 		// end-user-code
 	}
+	
+	/** 
+	 * @param l
+	 */
+	public void addMapListener(MapListener l) {
+		//TODO
+	}
+
+	/** 
+	 * @param L
+	 */
+	public void removeMapListener(MapListener L) {
+		// TODO
+	}
+	
+	
 }
