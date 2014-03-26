3f424228649eb9e8c891f8304cf0d5d6d75dc218
leorocher <rocher.lr@gmail.com>
2014-03-26 14:42:09 +0100

    Implémentation controller
    Implémentation d'une premiere version de MainController

--- src/main/java/org/ICE/PDC/antman/controller/MainCtrl.java
+++ src/main/java/org/ICE/PDC/antman/controller/MainCtrl.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.ICE.PDC.antman.controller;
 
 import org.ICE.PDC.antman.model.Monde;
@@ -11,7 +8,12 @@ public class MainCtrl implements MainFrameListener {
 
 	private MainFrame mainFrame;
 	private Monde monde;
-    
+    private long wait = 0;
+    private boolean meteoSet = false;
+    private boolean abondanceSet = false;
+    private int meteo = 0;
+    private int abondance = 0;
+	private long executionTime = 0;
     
 	public MainCtrl(Monde monde){
 		this.monde = monde;
@@ -33,19 +35,51 @@ public class MainCtrl implements MainFrameListener {
 	}
 
 	public void setVitesse(int vitesse) {
-		
+		if(vitesse > 0) {
+			this.wait = (1/vitesse)*3000;
+		} else {
+			this.wait = 0;
+		}
 	}
 
 	public void jouerTour() {
-		System.out.println("...TODO...");
+		
+		try {
+			
+			if(this.wait-this.executionTime > 0) {
+				Thread.sleep(this.wait-this.executionTime);
+			}
+			
+			long startTime = System.currentTimeMillis();
+			
+			this.monde.jouerTour();
+			
+			if(this.abondanceSet) {
+				this.monde.setAbondance(this.abondance);
+				this.abondanceSet = false;
+			}
+			
+			if(this.meteoSet) {
+				this.monde.setMeteo(this.meteo);
+				this.meteoSet = false;
+			}
+			
+			this.executionTime  = System.currentTimeMillis()-startTime;
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	public void setMeteo(int meteo) {
-		
+		this.meteoSet = true;
+		this.meteo = meteo;
 	}
 
 	public void setAbondance(int abondance) {
-		
+		this.abondanceSet = true;
+		this.abondance = abondance;
 	}
 	
 }
