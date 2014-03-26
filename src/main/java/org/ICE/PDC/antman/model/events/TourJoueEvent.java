8090be005a9f3f2cc11e3ce10394eed68a8247a7
alexisdevise <alexis.devise@gmail.com>
2014-03-26 14:26:35 +0100

    ....

--- src/main/java/org/ICE/PDC/antman/model/events/TourJoueEvent.java
+++ src/main/java/org/ICE/PDC/antman/model/events/TourJoueEvent.java
@@ -4,12 +4,12 @@ import java.util.Date;
 
 import org.ICE.PDC.antman.model.Monde;
 
-public class TourJouerEvent extends MapEvent {
+public class TourJoueEvent extends MapEvent {
 
 
 	private final Monde monde;
 	
-	public TourJouerEvent(int tour, Date datetime,Monde monde) {
+	public TourJoueEvent(int tour, Date datetime,Monde monde) {
 		super(tour, datetime);
 		this.monde = monde;
 	}
